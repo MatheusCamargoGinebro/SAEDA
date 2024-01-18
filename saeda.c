7f9f459b2bc0d6fe6604b076e50edfc3a49e1745
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2024-01-18 15:48:54 -0300

    saeda 1.0

---
+++ saeda.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <windows.h>
+
+void pause(){
+    system("pause");
+}
+
+void clear(){
+    system("cls");
+}
+
+
+
+int main(){
+    printf("O======================================================O\n");
+    printf("| SAEDA - Sistema de Automacao de Abertura de Arquivos |\n");
+    printf("O------------------------------------------------------O\n");
+    printf("| ver 1.0 18/01/2024 - Testes e impressoes.            |\n");
+    printf("O======================================================O\n");
+
+
+    return 0;
+}
