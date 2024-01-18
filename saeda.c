3626618968b1786e4f3a7282e8093524a9ae17d4
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2024-01-18 16:08:35 -0300

    Add file input functionality for executing a list of files

--- saeda.c
+++ saeda.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 
+// Funções de interface:
 void pause(){
     system("pause");
 }
@@ -9,6 +10,7 @@ void clear(){
     system("cls");
 }
 
+// Funções de sistema:
 
 
 int main(){
@@ -18,6 +20,22 @@ int main(){
     printf("| ver 1.0 18/01/2024 - Testes e impressoes.            |\n");
     printf("O======================================================O\n");
 
+    printf("\nDigite o caminho do arquivo de lista de arquivos a serem executados.\nR: ");
+    char fileName[1024];
+    scanf("%s", fileName);
+    
+    FILE *file = fopen(fileName, "r");
 
+    if(file == NULL){
+        printf("Erro ao procurar lista de caminhos.\n");
+        pause();
+        return 1;
+    } else {
+        printf("lista de caminhos encontrada!\n");
+        pause();
+
+        // configurando para permitir que o programa execute os arquivos como administrador:
+        
+    }
     return 0;
 }
